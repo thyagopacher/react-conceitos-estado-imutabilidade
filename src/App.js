7e131e849438286a439e4ced1cf5f64075b88317
Thyago  Henrique Pacher <thyago.pacher@gmail.com>
2020-12-26 17:03:49 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+import Header from './components/Header';
+
+/**
+ * Conceitos principais para React 
+ * Componente
+ * Propriedade
+ * Estado e Imutabilidade
+ *  */
+function App() {
+    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+    /**
+     * useState retorna um array com 2 posições.
+     * 
+     * 1. Variável com seu valor inicial
+     * 2. Função para atualizarmos esse valor
+     */
+    function handleAddProject(){
+        setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    }
+
+    return (
+        <>
+            <Header title="Projects"/>
+
+            <ul>
+                {projects.map(project => <li key={project}>{project}</li>)}
+            </ul>
+
+            <button type='button' onClick={handleAddProject}>Adicionar projeto</button>
+        </>
+    );
+}
+
+export default App;
